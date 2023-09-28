5646b8b6da8aca5df1944024e72f9d881cd39a56
tangoyankee <miller.tim108@gmail.com>
2023-09-28 12:28:39 -0400

    create migrate script

---
+++ api/drizzle/migrate.ts
@@ -0,0 +1,20 @@
+import { migrate } from 'drizzle-orm/node-postgres/migrator';
+import { drizzle } from 'drizzle-orm/node-postgres';
+import { Client } from 'pg';
+import { exit } from 'process';
+
+(async () => {
+  console.info('start migrate');
+  const client = new Client({
+    host: process.env.DB_HOST,
+    port: parseInt(process.env.DB_PORT),
+    user: process.env.DB_USER,
+    password: process.env.DB_PASSWORD,
+    database: process.env.DB_NAME,
+  });
+  await client.connect();
+  const db = drizzle(client);
+  await migrate(db, { migrationsFolder: './drizzle/migration' });
+  console.info('end migrate');
+  exit();
+})();
