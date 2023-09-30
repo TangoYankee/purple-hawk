d39a62f306d52853960b923fec2e76aa6b23e904
tangoyankee <miller.tim108@gmail.com>
2023-09-30 18:49:31 -0400

    implement crud for tax-lot

--- api/src/tax-lot/tax-lot.controller.ts
+++ api/src/tax-lot/tax-lot.controller.ts
@@ -1,6 +1,16 @@
-import { Body, Controller, Get, Post } from '@nestjs/common';
+import {
+  Body,
+  Controller,
+  Delete,
+  Get,
+  HttpCode,
+  Param,
+  Patch,
+  Post,
+} from '@nestjs/common';
 import { TaxLotService } from './tax-lot.service';
 import { InsertTaxLot } from 'src/schema/tax-lot';
+import { Polygon } from 'geojson';
 
 @Controller('tax-lot')
 export class TaxLotController {
@@ -11,8 +21,25 @@ export class TaxLotController {
     return this.service.getAll();
   }
 
+  @Get('/:id')
+  async getById(@Param('id') id: string) {
+    return this.service.getById(id);
+  }
+
   @Post('/')
-  async create(@Body() params: InsertTaxLot) {
+  async create(@Body() params: InsertTaxLot): Promise<{ id: string }> {
     return await this.service.create(params);
+  }
+
+  @Patch('/:id')
+  async update(@Param('id') id: string, @Body() params: { geom: Polygon }) {
+    return await this.service.update(id, params);
+  }
+
+  @Delete('/:id')
+  @HttpCode(204)
+  async delete(@Param('id') id: string) {
+    await this.service.delete(id);
+    return null;
   }
 }
--- api/src/tax-lot/tax-lot.service.ts
+++ api/src/tax-lot/tax-lot.service.ts
@@ -1,5 +1,5 @@
 import { Inject, Injectable } from '@nestjs/common';
-import { sql } from 'drizzle-orm';
+import { eq, sql } from 'drizzle-orm';
 import { Polygon } from 'src/drizzle-pgis/types';
 import { DbType, DB } from 'src/global/providers/db.provider';
 import { SelectTaxLot, InsertTaxLot, taxLot } from 'src/schema/tax-lot';
@@ -21,11 +21,25 @@ export class TaxLotService {
     }));
   }
 
+  async getById(id: string): Promise<SelectTaxLot> {
+    const result = await this.db
+      .select({
+        id: taxLot.id,
+        geom: sql<string>`ST_AsGeoJSON(${taxLot.geom})`,
+      })
+      .from(taxLot)
+      .where(eq(taxLot.id, id));
+    return result.map(({ id, geom }) => ({
+      id,
+      geom: JSON.parse(geom) as Polygon,
+    }))[0];
+  }
+
   async create(row: InsertTaxLot): Promise<{ id: string }> {
     const encodingQuery = await this.db.execute(
-      sql<string>`select ST_GeomFromGeoJSON(${row.geom}) as enconded_geom`,
+      sql<string>`select ST_GeomFromGeoJSON(${row.geom}) as encoded_geom`,
     );
-    const encodedGeom = encodingQuery.rows[0].enconded_geom;
+    const encodedGeom = encodingQuery.rows[0].encoded_geom;
     const result = await this.db
       .insert(taxLot)
       .values({
@@ -34,5 +48,26 @@ export class TaxLotService {
       })
       .returning({ id: taxLot.id });
     return result[0];
+  }
+
+  async update(id: string, values: { geom: Polygon }): Promise<{ id: string }> {
+    const encodingQuery = await this.db.execute(
+      sql<string>`select ST_GeomFromGeoJSON(${values.geom}) as encoded_geom`,
+    );
+    const encodedGeom = encodingQuery.rows[0]
+      .encoded_geom as unknown as Polygon;
+    console.info('encoded', encodedGeom);
+    const result = await this.db
+      .update(taxLot)
+      .set({
+        geom: encodedGeom,
+      })
+      .where(eq(taxLot.id, id))
+      .returning({ id: taxLot.id });
+    return result[0];
+  }
+
+  async delete(id: string): Promise<void> {
+    await this.db.delete(taxLot).where(eq(taxLot.id, id));
   }
 }
