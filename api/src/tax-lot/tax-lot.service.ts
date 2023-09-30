2a8c01e9e2b38a5d1564add2930e8cf4346eb951
tangoyankee <miller.tim108@gmail.com>
2023-09-29 22:19:36 -0400

    select all tax lots with geom

--- api/src/tax-lot/tax-lot.service.ts
+++ api/src/tax-lot/tax-lot.service.ts
@@ -1,11 +1,23 @@
-import { Inject } from '@nestjs/common';
+import { Inject, Injectable } from '@nestjs/common';
+import { sql } from 'drizzle-orm';
+import { Point } from 'src/drizzle-pg-geotype';
 import { DbType, DB } from 'src/global/providers/db.provider';
 import { SelectTaxLot, taxLot } from 'src/schema/tax-lot';
 
+@Injectable()
 export class TaxLotService {
   constructor(@Inject(DB) private readonly db: DbType) {}
 
   async getAll(): Promise<Array<SelectTaxLot>> {
-    return this.db.select().from(taxLot);
+    const result = await this.db
+      .select({
+        id: taxLot.id,
+        geom: sql<string>`ST_AsGeoJSON(${taxLot.geom})`,
+      })
+      .from(taxLot);
+    return result.map(({ id, geom }) => ({
+      id,
+      geom: JSON.parse(geom) as Point,
+    }));
   }
 }
