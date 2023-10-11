917ae787e9454535b99c507af534f015c7265b54
tangoyankee <miller.tim108@gmail.com>
2023-10-11 15:04:04 -0400

    restrict tax lot api methods to get

--- api/src/tax-lot/tax-lot.controller.ts
+++ api/src/tax-lot/tax-lot.controller.ts
@@ -1,15 +1,9 @@
 import {
-  Body,
   Controller,
-  Delete,
   Get,
-  HttpCode,
   Param,
-  Patch,
-  Post,
 } from '@nestjs/common';
 import { TaxLotService } from './tax-lot.service';
-import { Polygon } from '../../drizzle-pgis/types';
 
 @Controller('tax-lot')
 export class TaxLotController {
@@ -23,24 +17,5 @@ export class TaxLotController {
   @Get('/:id')
   async getById(@Param('id') id: string) {
     return this.service.getById(id);
-  }
-
-  @Post('/')
-  async create(
-    @Body() params: { id: string; wgs84: Polygon },
-  ): Promise<{ id: string }> {
-    return await this.service.create(params);
-  }
-
-  @Patch('/:id')
-  async update(@Param('id') id: string, @Body() params: { wgs84: Polygon }) {
-    return await this.service.update(id, params);
-  }
-
-  @Delete('/:id')
-  @HttpCode(204)
-  async delete(@Param('id') id: string) {
-    await this.service.delete(id);
-    return null;
   }
 }
--- api/src/tax-lot/tax-lot.service.ts
+++ api/src/tax-lot/tax-lot.service.ts
@@ -1,79 +1,32 @@
 import { Inject, Injectable } from '@nestjs/common';
-import { eq, sql } from 'drizzle-orm';
-import { Polygon } from '../../drizzle-pgis/types';
+import { eq } from 'drizzle-orm';
 import { DbType, DB } from 'src/global/providers/db.provider';
-import { projectSiteToTaxLot } from 'src/schema/project-site-tax-lot';
 import { taxLot } from 'src/schema/tax-lot';
 
 @Injectable()
 export class TaxLotService {
   constructor(@Inject(DB) private readonly db: DbType) {}
 
-  async getAll(): Promise<Array<{ id: string; wgs84: Polygon }>> {
+  // Limit results to 25 to avoid crashing application
+  async getAll(): Promise<Array<{ id: string; bbl: string }>> {
     const result = await this.db
       .select({
         id: taxLot.id,
-        wgs84: sql<string>`ST_AsGeoJSON(${taxLot.wgs84})`,
+        bbl: taxLot.bbl,
       })
-      .from(taxLot);
-    return result.map(({ id, wgs84 }) => ({
-      id,
-      wgs84: JSON.parse(wgs84) as Polygon,
-    }));
+      .from(taxLot)
+      .limit(25);
+    return result;
   }
 
-  async getById(id: string): Promise<{ id: string; wgs84: Polygon }> {
+  async getById(id: string): Promise<{ id: string; }> {
     const result = await this.db
       .select({
         id: taxLot.id,
-        wgs84: sql<string>`ST_AsGeoJSON(${taxLot.wgs84})`,
+        bbl: taxLot.bbl,
       })
       .from(taxLot)
       .where(eq(taxLot.id, id));
-    return result.map(({ id, wgs84 }) => ({
-      id,
-      wgs84: JSON.parse(wgs84) as Polygon,
-    }))[0];
-  }
-
-  async create(row: { id: string; wgs84: Polygon }): Promise<{ id: string }> {
-    const encodingQuery = await this.db.execute(
-      sql<string>`select ST_GeomFromGeoJSON(${row.wgs84})::geography as encoded_geog`,
-    );
-    const encodedGeog = encodingQuery.rows[0].encoded_geog;
-    const result = await this.db
-      .insert(taxLot)
-      .values({
-        id: row.id,
-        wgs84: encodedGeog as unknown as Polygon,
-      })
-      .returning({ id: taxLot.id });
     return result[0];
-  }
-
-  async update(
-    id: string,
-    values: { wgs84: Polygon },
-  ): Promise<{ id: string }> {
-    const encodingQuery = await this.db.execute(
-      sql<string>`select ST_GeomFromGeoJSON(${values.wgs84})::geography as encoded_geog`,
-    );
-    const encodedGeog = encodingQuery.rows[0]
-      .encoded_geom as unknown as Polygon;
-    const result = await this.db
-      .update(taxLot)
-      .set({
-        wgs84: encodedGeog,
-      })
-      .where(eq(taxLot.id, id))
-      .returning({ id: taxLot.id });
-    return result[0];
-  }
-
-  async delete(id: string): Promise<void> {
-    await this.db
-      .delete(projectSiteToTaxLot)
-      .where(eq(projectSiteToTaxLot.taxLotId, id));
-    await this.db.delete(taxLot).where(eq(taxLot.id, id));
   }
 }
