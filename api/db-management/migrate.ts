80629dab86fbf6543cc8268fd71d9ad4078d9bba
tangoyankee <miller.tim108@gmail.com>
2023-10-11 09:49:46 -0400

    give pg client its own file

--- api/db-management/migrate.ts
+++ api/db-management/migrate.ts
@@ -1,19 +1,12 @@
 import { migrate } from 'drizzle-orm/node-postgres/migrator';
 import { drizzle } from 'drizzle-orm/node-postgres';
-import { Client } from 'pg';
 import { exit } from 'process';
+import { pgClient } from './pg-client';
 
 (async () => {
   console.info('start migrate');
-  const client = new Client({
-    host: process.env.DB_HOST,
-    port: parseInt(process.env.DB_PORT),
-    user: process.env.DB_USER,
-    password: process.env.DB_PASSWORD,
-    database: process.env.DB_NAME,
-  });
-  await client.connect();
-  const db = drizzle(client);
+  await pgClient.connect();
+  const db = drizzle(pgClient);
   await migrate(db, { migrationsFolder: './db-management/migration' });
   console.info('end migrate');
   await pgClient.end();
---
+++ api/db-management/pg-client.ts
@@ -0,0 +1,9 @@
+import { Client } from 'pg';
+
+export const pgClient = new Client({
+  host: process.env.DB_HOST,
+  port: parseInt(process.env.DB_PORT),
+  user: process.env.DB_USER,
+  password: process.env.DB_PASSWORD,
+  database: process.env.DB_NAME,
+});
