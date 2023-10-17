90ad7a038b5ac660c6d77c5a5688b9ee7869c89b
tangoyankee <miller.tim108@gmail.com>
2023-10-17 15:39:50 -0400

    exclude spatial fields from tax lot endpoint

--- api/src/tax-lot/tax-lot.service.ts
+++ api/src/tax-lot/tax-lot.service.ts
@@ -8,14 +8,67 @@ export class TaxLotService {
   constructor(@Inject(DB) private readonly db: DbType) {}
 
   // Limit results to 2 to avoid crashing application
-  async getAll(): Promise<Array<SelectTaxLot>> {
-    const result = await this.db.select().from(taxLot).limit(2);
+  async getAll(): Promise<Array<Partial<SelectTaxLot>>> {
+    const result = await this.db
+      .select({
+        bbl: taxLot.bbl,
+        boroughCode: taxLot.boroughCode,
+        block: taxLot.block,
+        lot: taxLot.lot,
+        address: taxLot.address,
+        buildingClass: taxLot.buildingClass,
+        landUseCode: taxLot.landUseCode,
+        commercialArea: taxLot.commercialArea,
+        residentialArea: taxLot.residentialArea,
+        officeArea: taxLot.officeArea,
+        retailArea: taxLot.residentialArea,
+        garageArea: taxLot.garageArea,
+        storageArea: taxLot.storageArea,
+        factoryArea: taxLot.factoryArea,
+        otherArea: taxLot.otherArea,
+        floors: taxLot.floors,
+        residentialUnits: taxLot.residentialUnits,
+        totalUnits: taxLot.totalUnits,
+        lotType: taxLot.lotType,
+        landmark: taxLot.landmark,
+        builtFAR: taxLot.builtFAR,
+        residentialFAR: taxLot.residentialFAR,
+        commercialFAR: taxLot.commercialFAR,
+        facilityFAR: taxLot.facilityFAR,
+      })
+      .from(taxLot)
+      .limit(2);
     return result;
   }
 
-  async getById(bbl: string): Promise<SelectTaxLot> {
+  async getById(bbl: string): Promise<Partial<SelectTaxLot>> {
     const result = await this.db
-      .select()
+      .select({
+        bbl: taxLot.bbl,
+        boroughCode: taxLot.boroughCode,
+        block: taxLot.block,
+        lot: taxLot.lot,
+        address: taxLot.address,
+        buildingClass: taxLot.buildingClass,
+        landUseCode: taxLot.landUseCode,
+        commercialArea: taxLot.commercialArea,
+        residentialArea: taxLot.residentialArea,
+        officeArea: taxLot.officeArea,
+        retailArea: taxLot.residentialArea,
+        garageArea: taxLot.garageArea,
+        storageArea: taxLot.storageArea,
+        factoryArea: taxLot.factoryArea,
+        otherArea: taxLot.otherArea,
+        floors: taxLot.floors,
+        residentialUnits: taxLot.residentialUnits,
+        totalUnits: taxLot.totalUnits,
+        lotType: taxLot.lotType,
+        landmark: taxLot.landmark,
+        builtFAR: taxLot.builtFAR,
+        residentialFAR: taxLot.residentialFAR,
+        commercialFAR: taxLot.commercialFAR,
+        facilityFAR: taxLot.facilityFAR,
+      })
       .from(taxLot)
       .where(eq(taxLot.bbl, bbl));
     return result[0];
